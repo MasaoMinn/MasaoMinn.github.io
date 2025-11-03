5e222998912ffdb0ccde16790eb7db5b88dd94c5
masaominn <15824014+masaominn@user.noreply.gitee.com>
2025-11-03 14:13:37 +0800

    update /furry

--- src/app/Furry/page.tsx
+++ src/app/Furry/page.tsx
@@ -3,20 +3,31 @@ import React from 'react';
 import { Col, Container, Row, Image, Accordion } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ImgCarousel from './Carousel';
+import { useTranslation } from 'react-i18next'
+import { introStyle } from './style';
+import { useTheme, darkTheme, lightTheme } from '@/components/boxed/ThemeProvider';
 const QQProfileLink: React.FC = () => {
   const qqNumber = '2134361910'; // QQ号
   const QQUrl = `https://qm.qq.com/q/QTfus78dqe`;// QQ主页URL
+  const { theme } = useTheme();
+
+  const { t } = useTranslation();
   return (
-    <Container fluid className="text-center px-3" style={{ marginTop: '20px' }}>
+    <Container fluid className="text-center px-3" style={theme === 'dark' ? darkTheme : lightTheme}>
       <Row className="justify-content-center g-1">
         <Col
           xs={12}
           lg={8}
           xl={6}
-          className="justify-content-center"
+          className="justify-content-center mt-3"
           style={{ border: "2px solid blue", borderRadius: "10px", padding: "10px", backgroundColor: "white" }}
         >
           <ImgCarousel />
+        </Col>
+      </Row>
+      <Row className="justify-content-center g-1">
+        <Col xs={10} md={9} lg={8} className="d-flex justify-content-center">
+          <p style={introStyle}>{t('furry.intro')}</p>
         </Col>
       </Row>
 
@@ -50,7 +61,7 @@ const QQProfileLink: React.FC = () => {
       <Row className="justify-content-center g-1">
         <Col xs={9} md={6} className="d-flex justify-content-center">
           <Button variant="info" href="https://www.xiaohongshu.com/user/profile/649132e3000000000f004dc5?xhsshare=userQrCode" aria-label={`访问小红书：5514710200`} className="px-4 py-2" >
-            <img src='xiaohongshuicon.png' alt="小红书图标" width="20" height="20" className="me-2" />
+            <Image src='xiaohongshuicon.png' alt="小红书图标" width="20" height="20" className="me-2" />
             点击跳转我的小红书
           </Button>
         </Col>
@@ -77,7 +88,7 @@ const QQProfileLink: React.FC = () => {
       <Row className="justify-content-center g-0">
         <Col xs={9} md={6} className="d-flex justify-content-center">
           <Button variant="info" href="https://www.douyin.com/user/MS4wLjABAAAA3ZnB6dr1lknUupJiF0XZrWZ1mUtsvpRJfuSgmT94WRJpfkvO5S4Jja5h4yFo9vyM?from_tab_name=main" aria-label={`访问小红书：5514710200`} className="px-4 py-2" >
-            <img src='douyinicon.png' alt="抖音图标" width="20" height="20" className="me-2" />
+            <Image src='douyinicon.png' alt="抖音图标" width="20" height="20" className="me-2" />
             点击跳转我的抖音
           </Button>
         </Col>
