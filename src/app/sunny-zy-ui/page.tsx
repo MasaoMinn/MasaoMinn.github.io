76bc3d619615a1b68a08328d4d8a291e4011dfe9
masaominn <2134361910@qq.com>
2026-04-07 17:13:56 +0800

    解决打包错误

---
+++ src/app/sunny-zy-ui/page.tsx
@@ -0,0 +1,8 @@
+export default function SunnyZyUiPage() {
+  return (
+    <main style={{ padding: "2rem 1rem", textAlign: "center" }}>
+      <h1>Sunny ZY UI</h1>
+      <p>UI playground page is ready.</p>
+    </main>
+  );
+}
